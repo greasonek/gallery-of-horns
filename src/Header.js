e0f29cea89ad558367f95b6a18887aefb374b3af
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-04-15 16:03:48 -0700

    completed class 01 lab

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component{
   render(){
     return(
     <>
-      <h1>Students of 301</h1>
+      <h1>Gallery of Horns</h1>
     </>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Student from "./Student";
+import HornedBeast from "./HornedBeast";
 
 class Main extends React.Component{
   render(){
     return(
     <>
-      <p>Hello World!</p>
-      <Student name={'Emily'}/>
-      <Student name={'Josh'}/>
-      <Student name={'Lacey'}/>
+      <p>Gallery of Horns</p>
+      <HornedBeast title={'UniWal'} imgUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} alt='A unicorn and a narwhal nuzzling their horns' description={'A unicorn and a narwhal nuzzling their horns'}/>
+      <HornedBeast title={'Rhino Family'} imgUrl={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'} alt='Parent rhino with two babies' description={'Parent rhino with two babies'}/>
+      <HornedBeast title={'Unicorn Head'} imgUrl={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'} alt='Someone wearing a very silly unicorn head mask' description={'Someone wearing a very silly unicorn head mask'}/>
     </>
     )
   }
